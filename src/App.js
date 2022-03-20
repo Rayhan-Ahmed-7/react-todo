bf1fa24b35b59335e7fff5ea433b87f76a6b92a9
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-03-21 00:43:32 +0600

    tut

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hey</h1>
+      <div className="top-content">
+        <header>
+            <h2>Todo List App</h2>
+        </header>
+        <div className="select">
+            <select name="todos" id="" className="filter-todo">
+                <option value="all">All</option>
+                <option value="selected">Selected</option>
+                <option value="unselected">Unselected</option>
+            </select>
+        </div>
+        <form action="">
+            <input type="text" className="todo-input" />
+            <button className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+        </form>
+      </div>
     </div>
   );
 }
